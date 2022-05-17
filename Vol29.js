31f86311597e441261034f9fb2861492aee5985c
Dylan COVAREL <102042578+DylanCOVAREL@users.noreply.github.com>
2022-05-17 15:39:52 +0200

    Add files via upload

--- Vol29.js
+++ Vol29.js
@@ -1,18 +1,26 @@
+//-----------------------------------------------DESSIN TABLEAU--------------------------------------------------------------//
+
 function Tableau2D(x, y) {
     var array2D = new Array(x);
     for (var i = 0; i < array2D.length; i++) {
-    array2D[i] = new Array(y);
+        array2D[i] = new Array(y);
     }
     return array2D;
 }
 
 let monTableau = new Tableau2D(10, 10);
-monTableau[5][2] = "Tresor" ;
 
-
-
-
-//-----------------------------------------------DESSIN TABLEAU--------------------------------------------------------------//
-
-let unTexte = "<p>Code <strong>HTML</strong> généré via JavaScript.</p>"
-document.getElementById("ici").value = unTexte
+function drawTableau2D(x, y, emplacement) {
+    let idString = ""
+    let texte = "<table>"
+    for (let xx=0; xx < x; xx++) {
+        texte += `<tr>`
+        for (let yy=0; yy < y; yy++) {
+            idString = xx+"-"+yy
+            texte += `<td id="${idString}"></td>`
+        }
+        texte += `</tr>`
+    }
+    texte += `</table>`
+    document.getElementById(emplacement).innerHTML = texte
+}
